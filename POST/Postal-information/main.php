f79a534443b78e41f9cf92f4c32ca0ce656877e7
niiharamegumu <nhrmgm1223@gmail.com>
2018-11-06 15:07:32 +0900

    fix.

--- POST/Postal-information/main.php
+++ POST/Postal-information/main.php
@@ -28,20 +28,21 @@ fclose( $fp );
     <thead>
       <tr><th>郵便番号</th><th>都道府県</th><th>市区町村</th><th>町域</th></tr>
     </thead>
-    <?php foreach ($data as $value): ?>
-      <?php
-        preg_match('/[0-9]{3}/', $value[2],$pre);
-        preg_match('/[0-9]{4}$/', $value[2],$after);
-        $html = '';
-        $html .= '<tr>';
-        $html .= '<td>' . $pre[0] . '-' . $after[0] . '</td>';
-        $html .= '<td>' . $value[6] . '</td>';
-        $html .= '<td>' . $value[7] . '</td>';
-        $html .= '<td>' . $value[8] . '</td>';
-        $html .= '</tr>';
-        echo $html;
-      ?>
-    <?php endforeach; ?>
+    <?php
+        foreach ($data as $value) {
+          preg_match('/[0-9]{3}/', $value[2], $pre);
+          preg_match('/[0-9]{4}$/', $value[2], $after);
+          
+          $html = '';
+          $html .= '<tr>';
+          $html .= '<td>' . $pre[0] . '-' . $after[0] . '</td>';
+          $html .= '<td>' . $value[6] . '</td>';
+          $html .= '<td>' . $value[7] . '</td>';
+          $html .= '<td>' . $value[8] . '</td>';
+          $html .= '</tr>';
+          echo $html;
+        }
+    ?>
   </table>
 </body>
 </html>
