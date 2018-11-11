a6afa8b10d8c1f38ebc2c025b7c2bcd54a92e615
niiharamegumu <nhrmgm1223@gmail.com>
2018-11-12 01:27:40 +0900

    Add Postal Search.

--- mySQL/postalSearch/main.php
+++ mySQL/postalSearch/main.php
@@ -1,63 +1,86 @@
 <?php
-$pref_data = array(
-        '北海道','青森県','岩手県','宮城県','秋田県','山形県','福島県','茨城県','栃木県','群馬県',
-        '埼玉県','千葉県','東京都','神奈川県','新潟県','富山県','石川県','福井県','山梨県','長野県',
-        '岐阜県','静岡県','愛知県','三重県','滋賀県','京都府','大阪府','兵庫県','奈良県','和歌山県',
-        '鳥取県','島根県','岡山県','広島県','山口県','徳島県','香川県','愛媛県','高知県','福岡県',
-        '佐賀県','長崎県','熊本県','大分県','宮崎県','鹿児島県','沖縄県');
-$host = 'localhost';
-$userName = 'root';
-$userPw = 'root';
-$dbName = 'code_camp';
-$link = mysqli_connect( $host, $userName, $userPw, $dbName );
 
-if ( $link ) {
-  mysqli_set_charset( $link, 'utf8' );
-  $query_select = "SELECT  FROM ";
-
-} else {
-  echo 'データベースと接続できていません。';
-}
-
-
-
+require( 'postal_search.php' );
 
 ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
    <meta charset="UTF-8">
    <title>住所検索</title>
+   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
   <h1>郵便番号検索</h1>
-  <section>
+<div class="wrapper">
+  <section class="search-form">
     <h2>郵便番号から検索</h2>
     <form>
       <input type="text" name="number" placeholder="例）1010001" value="">
+      <input type="hidden" name="search" value="number"><br>
       <input type="submit" value="検索">
     </form>
     <h2>地名から検索</h2>
     <form>
-      都道府県を選択
+      都道府県を選択<br>
       <select name="pref">
         <option value="" selected>都道府県を選択</option>
         <?php
-          foreach ($pref_data as $value) {
-            $html = "";
-            $html .= "<option value='" . $value . "'>";
-            $html .= $value;
-            $html .= "</option>";
-            echo $html;
-          }
+        foreach ($pref_data as $value) {
+          $html = "";
+          $html .= "<option value='" . $value . "'>";
+          $html .= $value;
+          $html .= "</option>\n";
+          echo $html;
+        }
         ?>
-      </select>
-      <input type="text" name="address" value="" placeholder="ﾐﾅﾄｸ">
+      </select><br>
+      市区町村(全角カタカナ・ひらがな/半角ｶﾀｶﾅ)<br>
+      <input type="text" name="city" value="" placeholder="ミヤザキシ">
+      <input type="hidden" name="search" value="adress"><br>
       <input type="submit" value="検索">
     </form>
   </section>
+
   <section class="search-reslut">
-    <p>検索結果0件</p>
-  </section>
+    <p>ここに結果が表示されます</p>
+    <?php if ( count($errors) > 0 ) : ?>
+      <?php
+      foreach ( $errors as $error ) {
+        echo '<p>' . $error . '</p>';
+      }
+      ?>
+    <?php elseif ( count($query_result_data) > 0 ) : ?>
+      <table>
+        <thead>
+          <tr>
+            <th>郵便番号</th>
+            <th>都道府県</th>
+            <th>市区町村</th>
+            <th>町域</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          foreach ( $query_result_data as $value ) {
+            $html = "";
+            $html .= "<tr>";
+              $html .= "<td>" . $value['number'] . "</td>";
+              $html .= "<td>" . $value['pref'] . "</td>";
+              $html .= "<td>" . $value['city'] . "</td>";
+              $html .= "<td>" . $value['town'] . "</td>";
+              $html .= "</tr>";
+              echo $html;
+            }
+            ?>
+          </tbody>
+        </table>
+      <?php else : ?>
+        <p>検索結果0件</p>
+      <?php endif; ?>
+    </section>
+</div>
+
 </body>
 </html>
---
+++ mySQL/postalSearch/postal_search.php
@@ -0,0 +1,69 @@
+<?php
+$pref_data = array(
+        '北海道','青森県','岩手県','宮城県','秋田県','山形県','福島県','茨城県','栃木県','群馬県',
+        '埼玉県','千葉県','東京都','神奈川県','新潟県','富山県','石川県','福井県','山梨県','長野県',
+        '岐阜県','静岡県','愛知県','三重県','滋賀県','京都府','大阪府','兵庫県','奈良県','和歌山県',
+        '鳥取県','島根県','岡山県','広島県','山口県','徳島県','香川県','愛媛県','高知県','福岡県',
+        '佐賀県','長崎県','熊本県','大分県','宮崎県','鹿児島県','沖縄県');
+$number = Null;
+$pref = Null;
+$city = Null;
+$errors = array();
+$query_result_data = array();
+
+
+$host = '';
+$userName = '';
+$userPw = '';
+$dbName = '';
+$link = mysqli_connect( $host, $userName, $userPw, $dbName );
+if ( $link ) {
+  mysqli_set_charset( $link, 'utf8' );
+  if ( isset($_GET['search']) ) {
+
+    if ( $_GET['search'] === 'number' ) {
+      if ( !isset($_GET['number']) || mb_strlen($_GET['number']) === 0 ) {
+        $errors['error_number'] = '郵便番号を入力してください。';
+      } elseif ( preg_match( "/[0-9]{7}/", $_GET['number'] ) !== 1 ) {
+        $errors['error_number'] = '半角数字7文字でお願いします。';
+      } else {
+        $number = htmlspecialchars($_GET['number'], ENT_QUOTES, 'UTF-8');
+        $number = "'" . $number . "'";
+        $query = "SELECT number,pref,city,town  FROM postal_search WHERE number=" . $number;
+        $result = mysqli_query( $link, $query );
+
+        while ( $record = mysqli_fetch_array( $result ) ) {
+          $query_result_data[] = $record;
+        }
+        mysqli_free_result( $result );
+        mysqli_close( $link );
+      }
+    }
+
+    if ( $_GET['search'] === 'adress' ) {
+      if ( !isset($_GET['pref']) || $_GET['pref'] === '' || mb_strlen($_GET['city']) === 0 ) {
+        $errors['error_pref'] = '都道府県を選択後、市区町村を記入ください';
+      } elseif ( preg_match( "/[ｦ-ﾟァ-ヴぁ-ん]+/", $_GET['city'] ) !== 1 ) {
+        $errors['error_pref'] = '半角ｶﾀｶﾅでお願いします';
+      } else {
+        $pref = htmlspecialchars($_GET['pref'], ENT_QUOTES, 'UTF-8');
+        $city = htmlspecialchars($_GET['city'], ENT_QUOTES, 'UTF-8');
+        $city = mb_convert_kana( $city, "kVh" );
+        $pref = "'" . $pref . "'";
+        $city = "'" . $city . "'";
+        $query = "SELECT number,pref,city,town  FROM postal_search WHERE pref=" . $pref . "AND city_kana=" . $city;
+        $result = mysqli_query( $link, $query );
+
+        while ( $record = mysqli_fetch_array( $result ) ) {
+          $query_result_data[] = $record;
+        }
+        mysqli_free_result( $result );
+        mysqli_close( $link );
+      }
+    }
+
+  }
+
+} else {
+  echo 'データベースと接続できていません。';
+}
