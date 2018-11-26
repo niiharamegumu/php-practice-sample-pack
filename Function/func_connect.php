ff690d0a3018d0bc5b9391344b3b7376741c9a35
niiharamegumu <nhrmgm1223@gmail.com>
2018-11-26 13:14:36 +0900

    Fix indent.

--- Function/func_connect.php
+++ Function/func_connect.php
@@ -4,6 +4,7 @@ define('TAX', 1.08);
 define('DB_HOST',   '');
 define('DB_PASSWD', '');
 define('DB_NAME',   '');
+define('DB_USER',   '');
 define('HTML_CHARACTER_SET', 'UTF-8');
 define('DB_CHARACTER_SET',   'UTF8');
 $goods_data = array();
@@ -11,16 +12,12 @@ $goods_data = array();
 
 $link = get_db_connect();
 
-// 商品の一覧を取得
 $goods_data = get_goods_table_list($link);
 
-// DB切断
 close_db_connect($link);
 
-// 商品の値段を税込みに変換
 $goods_data = price_before_tax_assoc_array($goods_data);
 
-// 特殊文字をHTMLエンティティに変換
 $goods_data = entity_assoc_array($goods_data);
 
 
@@ -39,14 +36,10 @@ function price_before_tax($price) {
 * @return array 税込み商品一覧配列データ
 */
 function price_before_tax_assoc_array($assoc_array) {
-
-    foreach ($assoc_array as $key => $value) {
-        // 税込み価格へ変換(端数は切り上げ)
-        $assoc_array[$key]['price'] = price_before_tax($assoc_array[$key]['price']);
-    }
-
-    return $assoc_array;
-
+  foreach ($assoc_array as $key => $value) {
+      $assoc_array[$key]['price'] = price_before_tax($assoc_array[$key]['price']);
+  }
+  return $assoc_array;
 }
 
 /**
@@ -55,7 +48,7 @@ function price_before_tax_assoc_array($assoc_array) {
 * @return str 変換後文字
 */
 function entity_str($str) {
-    return htmlspecialchars($str, ENT_QUOTES, HTML_CHARACTER_SET);
+  return htmlspecialchars($str, ENT_QUOTES, HTML_CHARACTER_SET);
 }
 
 /**
@@ -64,12 +57,12 @@ function entity_str($str) {
 * @return array 変換後配列
 */
 function entity_assoc_array($assoc_array) {
-    foreach ($assoc_array as $key => $value) {
-        foreach ($value as $keys => $values) {
-            $assoc_array[$key][$keys] = entity_str($values);
-        }
+  foreach ($assoc_array as $key => $value) {
+    foreach ($value as $keys => $values) {
+      $assoc_array[$key][$keys] = entity_str($values);
     }
-    return $assoc_array;
+}
+  return $assoc_array;
 }
 
 /**
@@ -77,16 +70,11 @@ function entity_assoc_array($assoc_array) {
 * @return obj $link DBハンドル
 */
 function get_db_connect() {
-
-    // コネクション取得
-    if (!$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWD, DB_NAME)) {
-        die('error: ' . mysqli_connect_error());
-    }
-
-    // 文字コードセット
-    mysqli_set_charset($link, DB_CHARACTER_SET);
-
-    return $link;
+  if (!$link = mysqli_connect(DB_HOST, DB_USER, DB_PASSWD, DB_NAME)) {
+    die('error: ' . mysqli_connect_error());
+  }
+  mysqli_set_charset($link, DB_CHARACTER_SET);
+  return $link;
 }
 
 /**
@@ -94,8 +82,7 @@ function get_db_connect() {
 * @param obj $link DBハンドル
 */
 function close_db_connect($link) {
-    // 接続を閉じる
-    mysqli_close($link);
+  mysqli_close($link);
 }
 
 /**
@@ -106,20 +93,16 @@ function close_db_connect($link) {
 * @return array 結果配列データ
 */
 function get_as_array($link, $sql) {
-    // 返却用配列
-    $data = array();
-    // クエリを実行する
-    if ($result = mysqli_query($link, $sql)) {
-        if (mysqli_num_rows($result) > 0) {
-            // １件ずつ取り出す
-            while ($row = mysqli_fetch_assoc($result)) {
-                $data[] = $row;
-            }
-        }
-        // 結果セットを開放
-        mysqli_free_result($result);
+  $data = array();
+  if ($result = mysqli_query($link, $sql)) {
+    if (mysqli_num_rows($result) > 0) {
+      while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+      }
     }
-    return $data;
+  mysqli_free_result($result);
+  }
+  return $data;
 }
 
 /**
@@ -129,42 +112,42 @@ function get_as_array($link, $sql) {
 * @return array 商品一覧配列データ
 */
 function get_goods_table_list($link) {
-    $sql = 'SELECT goods_name, price FROM goods_table';
-    return get_as_array($link, $sql);
+  $sql = 'SELECT goods_name, price FROM goods_table';
+  return get_as_array($link, $sql);
 }
 
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <title>ユーザー定義関数</title>
-    <style type="text/css">
-        table, td, th {
-            border: solid black 1px;
-        }
-        table {
-            width: 200px;
-        }
-        caption {
-            text-align: left;
-        }
-    </style>
+  <meta charset="UTF-8">
+  <title>ユーザー定義関数</title>
+  <style type="text/css">
+    table, td, th {
+        border: solid black 1px;
+    }
+    table {
+        width: 200px;
+    }
+    caption {
+        text-align: left;
+    }
+  </style>
 </head>
 <body>
-    <table>
-    <caption>商品一覧(税込み)</caption>
-        <tr>
-            <th>商品名</th>
-            <th>価格</th>
-        <tr>
-<?php foreach ($goods_data as $value) { ?>
-        <tr>
-            <td><?php print $value['goods_name']; ?></td>
-            <td><?php print $value['price']; ?></td>
-        </tr>
-<?php } ?>
-    </table>
+  <table>
+  <caption>商品一覧(税込み)</caption>
+    <tr>
+      <th>商品名</th>
+      <th>価格</th>
+    </tr>
+    <?php foreach ($goods_data as $value) { ?>
+    <tr>
+      <td><?php print $value['goods_name']; ?></td>
+      <td><?php print $value['price']; ?></td>
+    </tr>
+    <?php } ?>
+  </table>
 
 </body>
 </html>
