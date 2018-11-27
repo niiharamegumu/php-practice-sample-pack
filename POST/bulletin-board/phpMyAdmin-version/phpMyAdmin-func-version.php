3faf23fc403cb02defb311d074841254807d9d10
niiharamegumu <nhrmgm1223@gmail.com>
2018-11-27 16:14:06 +0900

    commit.

--- POST/bulletin-board/phpMyAdmin-version/phpMyAdmin-func-version.php
+++ POST/bulletin-board/phpMyAdmin-version/phpMyAdmin-func-version.php
@@ -4,10 +4,10 @@ $one_board_data = array();
 define('NAME_MAX', 20);
 define('COMMENT_MAX', 100);
 
-define('DB_HOST',   'localhost');
-define('DB_USER',   'root');
-define('DB_PASSWD', 'root');
-define('DB_NAME',   'one_board');
+define('DB_HOST',   '');
+define('DB_USER',   '');
+define('DB_PASSWD', '');
+define('DB_NAME',   '');
 define('DB_CHARACTER_SET',   'UTF8');
 define('HTML_CHARACTER_SET', 'UTF-8');
 
