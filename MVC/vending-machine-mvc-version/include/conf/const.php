7f2ed1c48ca7b0a11e46ab256d6d1b86eaab37bc
niiharamegumu <nhrmgm1223@gmail.com>
2018-11-29 16:12:36 +0900

    2018 11 29 commit.

--- MVC/vending-machine-mvc-version/include/conf/const.php
+++ MVC/vending-machine-mvc-version/include/conf/const.php
@@ -1,9 +1,9 @@
 <?php
 $err_msg = array();
 
-define('DB_HOST',   'localhost');
-define('DB_USER',   'root');
-define('DB_PASSWD', 'root');
-define('DB_NAME',   'vending_machine');
+define('DB_HOST',   '');
+define('DB_USER',   '');
+define('DB_PASSWD', '');
+define('DB_NAME',   '');
 define('DB_CHARACTER_SET',   'UTF8');
 define('HTML_CHARACTER_SET', 'UTF-8');
